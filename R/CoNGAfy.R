15b0751864d3b6fba040c17aa9c6aaee46e0e8ff
theHumanBorch <ncborch@gmail.com>
2026-01-06 08:10:41 -0600

    Update CoNGAfy.R

--- R/CoNGAfy.R
+++ R/CoNGAfy.R
@@ -1,69 +1,84 @@
-#' Reduce a single-cell object to representative cells
-#' 
-#' Generate a single-cell object that has a representation of
-#' RNA expression by clonotype. This approach was first introduced
-#' in \href{https://pubmed.ncbi.nlm.nih.gov/34426704/}{CoNGA} and was 
-#' adapted to R. Please read and cite the author's work. 
-#' 
+#' Reduce single-cell object to representative cells by clonotype
+#'
+#' Generate a single-cell object that has a single representative cell
+#' for each clonotype. This approach was first introduced in
+#' \href{https://pubmed.ncbi.nlm.nih.gov/34426704/}{CoNGA} and was
+#' adapted to R. Please read and cite the original work.
+#'
+#' @param sc Single-cell object in Seurat or SingleCellExperiment format.
+#'   Must contain TCR information added via scRepertoire.
+#' @param method Character. Method for selecting representative cells:
+#'   \itemize{
+#'     \item \code{"mean"}: Generate mean expression values across all cells
+#'       sharing the same clonotype
+#'     \item \code{"dist"}: Select the cell with minimal Euclidean distance
+#'       from the clonotype centroid in PCA space
+#'   }
+#' @param features Character vector. Genes to include in the output.
+#'   Default (\code{NULL}) returns all genes.
+#' @param assay Character. Name of the assay(s) to return. Default is
+#'   \code{"RNA"}. Can be a vector for multiple assays.
+#' @param meta.carry Character vector. Metadata variables to carry over from
+#'   the original single-cell object. Default is \code{c("CTaa", "CTgene")}.
+#'
+#' @return Single-cell object with one cell representing each unique clonotype.
+#'
+#' @seealso \code{\link{runTrex}} for embedding the reduced object;
+#'   \code{\link{annotateDB}} for epitope annotation
+#'
 #' @examples
-#' trex_values <- CoNGAfy(trex_example, 
-#'                        method = "dist",
-#'                        features = NULL)
-#'                         
-#' @param input.data Single Cell Object in Seurat or Single Cell Experiment format
-#' @param method "mean" or "dist" Generate a mean value across features by clonotype or 
-#' use the PCA reduction to identify the cell with the minimal euclidean distance from the
-#' clonotype group.
-#' @param features Selected genes for the reduction DEFAULT: null will return all genes
-#' @param assay The name of the assay or assays to return.
-#' @param meta.carry Variables to carry over from the meta data of the single-cell object
-#' 
+#' CoNGA.seurat <- CoNGAfy(trex_example,
+#'     method = "dist",
+#'     features = NULL
+#' )
+#'
 #' @export
 #' @importFrom SeuratObject CreateSeuratObject CreateAssayObject
-#' @importFrom SingleCellExperiment SingleCellExperiment 
+#' @importFrom SingleCellExperiment SingleCellExperiment
 #' @importFrom SummarizedExperiment assay assay<- colData<-
-#' 
-#' @return Single-cell Object with one cell representing a single clone
-#' 
-#' 
-CoNGAfy <- function(input.data, 
-                    method = "dist", 
-                    features = NULL, 
-                    assay = "RNA", 
+CoNGAfy <- function(sc,
+                    method = "dist",
+                    features = NULL,
+                    assay = "RNA",
                     meta.carry = c("CTaa", "CTgene")) {
-    input.data <- filter.object(input.data)
+    sc <- filter.object(sc)
     conga <- NULL
-    if(method == "mean") {
+
+    if (method == "mean") {
         for (x in seq_along(assay)) {
-            conga[[x]] <- CoNGA.mean(input.data, features, assay[x])
-            
+            conga[[x]] <- CoNGA.mean(sc, features, assay[x])
         }
-    } else if(method == "dist") {
+    } else if (method == "dist") {
         for (x in seq_along(assay)) {
-            conga[[x]] <- CoNGA.dist(input.data, features, assay[x])
-            
+            conga[[x]] <- CoNGA.dist(sc, features, assay[x])
         }
-        
     }
+
     names(conga) <- assay
-    if (inherits(x=input.data, what ="Seurat")) {
-        sc.output <- CreateSeuratObject(conga[[1]], assay = names(conga)[1], project = "Trex")
-        if(length(conga) > 1) {
-            for(y in 2:length(conga)) {
+
+    if (inherits(x = sc, what = "Seurat")) {
+        sc.output <- CreateSeuratObject(
+            conga[[1]],
+            assay = names(conga)[1],
+            project = "Trex"
+        )
+        if (length(conga) > 1) {
+            for (y in 2:length(conga)) {
                 sc.output[[names(conga)[y]]] <- CreateAssayObject(conga[[y]])
             }
         }
-        CTge <- unique(input.data[[]][,c(meta.carry)])
-    } else if (inherits(x=input.data, what ="SingleCellExperiment")) {
+        CTge <- unique(sc[[]][, c(meta.carry)])
+    } else if (inherits(x = sc, what = "SingleCellExperiment")) {
         sc.output <- SingleCellExperiment(assay = conga[[1]])
-        if(length(conga) > 1) {
-            for(y in 2:length(conga)) {
+        if (length(conga) > 1) {
+            for (y in 2:length(conga)) {
                 assay(sc.output, names(conga)[y]) <- conga[[y]]
             }
         }
         sc.output$CTaa <- rownames(sc.output@colData)
-        CTge <- data.frame(unique(input.data@colData[,c(meta.carry)]))
+        CTge <- data.frame(unique(sc@colData[, c(meta.carry)]))
     }
+
     CTge <- meta.handler(CTge, meta.carry)
     clones <- unique(CTge$CTaa)
     rownames(CTge) <- clones
@@ -72,128 +87,190 @@ CoNGAfy <- function(input.data,
     return(sc.output)
 }
 
-
-
-#For all single clones, will use true RNA scaled values
-#For multiplets will use the cell with the minimal distance in PCA
-#For doublets, will automatically select the first cell. 
+#' Select representative cells by distance
+#'
+#' For single clones, uses actual RNA values. For multiplets, selects
+#' the cell with minimal Euclidean distance in PCA space. For doublets,
+#' automatically selects the first cell.
+#'
+#' @param sc Single-cell object
+#' @param features Features to include
+#' @param assay Assay name
+#'
+#' @return Sparse matrix of counts for representative cells
+#' @keywords internal
 #' @importFrom SummarizedExperiment assay
-CoNGA.dist <- function(input.data, features, assay) {
-    if (inherits(x=input.data, what ="Seurat")) {
-        if(assay == "RNA") {
-            data.use <- input.data[["pca"]]@cell.embeddings
+#' @importFrom SingleCellExperiment reducedDim
+CoNGA.dist <- function(sc, features, assay) {
+    if (inherits(x = sc, what = "Seurat")) {
+        if (assay == "RNA") {
+            data.use <- sc[["pca"]]@cell.embeddings
         } else if (assay == "ADT") {
-            data.use <- input.data[["apca"]]@cell.embeddings
+            data.use <- sc[["apca"]]@cell.embeddings
         }
-    } else if (inherits(x=input.data, what ="SingleCellExperiment")){
-        data.use <- reducedDim(input.data, "PCA")
+    } else if (inherits(x = sc, what = "SingleCellExperiment")) {
+        data.use <- reducedDim(sc, "PCA")
     }
-    meta <- grabMeta(input.data)
-    data <- as.data.frame(meta[,"CTaa"])
+
+    meta <- grabMeta(sc)
+    data <- as.data.frame(meta[, "CTaa"])
     colnames(data) <- "CTaa"
     rownames(data) <- rownames(meta)
+
     all.clones <- table(data$CTaa)
     unique.clones <- all.clones[which(all.clones > 1)]
     single.clones <- all.clones[all.clones %!in% unique.clones]
     barcodes <- rownames(data)[which(data$CTaa %in% names(single.clones))]
+
     for (i in seq_along(unique.clones)) {
         loc <- which(data$CTaa == names(unique.clones)[i])
-        dist <- as.matrix(dist(data.use[loc,]))
+        dist <- as.matrix(dist(data.use[loc, ]))
         cell <- names(which(rowSums(dist) == min(rowSums(dist))))
         if (length(cell) > 1) {
             cell <- cell[1]
         }
         barcodes <- c(barcodes, cell)
     }
-    if (inherits(x=input.data, what ="Seurat")) {
-        assay.use <- input.data[[assay]]$counts
-    } else if (inherits(x=input.data, what ="SingleCellExperiment")){
-        assay.use <- assay(input.data)
+
+    if (inherits(x = sc, what = "Seurat")) {
+        assay.use <- sc[[assay]]$counts
+    } else if (inherits(x = sc, what = "SingleCellExperiment")) {
+        assay.use <- assay(sc)
     }
+
     features.to.avg <- features %||% rownames(x = assay.use)
     features.assay <- intersect(x = features.to.avg, y = rownames(x = assay.use))
-    data.return <- assay.use[rownames(assay.use) %in% features.assay, colnames(assay.use) %in% barcodes]
+    data.return <- assay.use[
+        rownames(assay.use) %in% features.assay,
+        colnames(assay.use) %in% barcodes
+    ]
     colnames(data.return) <- data$CTaa[match(barcodes, rownames(data))]
     return(data.return)
 }
-# Adapted from the AverageExpression() function in Seurat
+
+#' Calculate mean expression by clonotype
+#'
+#' Adapted from the AverageExpression() function in Seurat.
+#'
+#' @param sc Single-cell object
+#' @param features Features to include
+#' @param assay Assay name
+#'
+#' @return Matrix of mean expression values by clonotype
+#' @keywords internal
 #' @importFrom rlang %||%
 #' @importFrom Matrix sparse.model.matrix colSums
 #' @importFrom SummarizedExperiment assay
 #' @importFrom stats as.formula
-CoNGA.mean <- function(input.data, features, assay) {
-    
-    if (inherits(x=input.data, what ="Seurat")) {
-        data.use <- input.data[[assay]]$counts
+CoNGA.mean <- function(sc, features, assay) {
+    if (inherits(x = sc, what = "Seurat")) {
+        data.use <- sc[[assay]]$counts
         data.use <- expm1(x = data.use)
-    } else if (inherits(x=input.data, what ="SingleCellExperiment")){
-        data.use <- assay(input.data, name = assay)
+    } else if (inherits(x = sc, what = "SingleCellExperiment")) {
+        data.use <- assay(sc, name = assay)
     }
+
     features.to.avg <- features %||% rownames(x = data.use)
     features.assay <- intersect(x = features.to.avg, y = rownames(x = data.use))
-    meta <- grabMeta(input.data)
-    data <- as.data.frame(meta[,"CTaa"])
+
+    meta <- grabMeta(sc)
+    data <- as.data.frame(meta[, "CTaa"])
     colnames(data) <- "CTaa"
     rownames(data) <- rownames(meta)
-    
-    #Modified to drop any cells without CTaa
-    data.use <- data.use[,!is.na(data[,"CTaa"])]
+
+    # Filter cells without CTaa
+    data.use <- data.use[, !is.na(data[, "CTaa"])]
     data <- data[which(rowSums(x = is.na(x = data)) == 0), , drop = FALSE]
-    
+
     for (i in seq_len(ncol(x = data))) {
         data[, i] <- as.factor(x = data[, i])
     }
-    num.levels <- sapply(X = seq_len(ncol(x = data)), FUN = function(i) { 
-        length(x = levels(x = data[, i]))
-    })
-    category.matrix <- sparse.model.matrix(object = as.formula(
-        object = paste0(
-            '~0+',
-            paste0(
-                "data[,",
-                1:length(x = "CTaa"),
-                "]",
-                collapse = ":"
+
+    num.levels <- vapply(
+        X = seq_len(ncol(x = data)),
+        FUN = function(i) {
+            length(x = levels(x = data[, i]))
+        },
+        FUN.VALUE = integer(1)
+    )
+
+    category.matrix <- sparse.model.matrix(
+        object = as.formula(
+            object = paste0(
+                "~0+",
+                paste0(
+                    "data[,",
+                    seq_len(length(x = "CTaa")),
+                    "]",
+                    collapse = ":"
+                )
             )
         )
-    ))
-    colsums <-Matrix::colSums(x = category.matrix)
+    )
+
+    colsums <- Matrix::colSums(x = category.matrix)
     category.matrix <- category.matrix[, colsums > 0]
     colsums <- colsums[colsums > 0]
-    
+
     category.matrix <- sweep(
         x = category.matrix,
         MARGIN = 2,
         STATS = colsums,
-        FUN = "/")
-    colnames(x = category.matrix) <- sapply(
+        FUN = "/"
+    )
+
+    colnames(x = category.matrix) <- vapply(
         X = colnames(x = category.matrix),
         FUN = function(name) {
-            name <- gsub(pattern = "data\\[, [1-9]*\\]", replacement = "", x = name)
-            return(paste0(rev(x = unlist(x = strsplit(x = name, split = ":"))), collapse = "_"))
-        })
+            name <- gsub(
+                pattern = "data\\[, [1-9]*\\]",
+                replacement = "",
+                x = name
+            )
+            return(paste0(
+                rev(x = unlist(x = strsplit(x = name, split = ":"))),
+                collapse = "_"
+            ))
+        },
+        FUN.VALUE = character(1)
+    )
+
     data.return <- data.use %*% category.matrix
     return(data.return)
 }
 
+#' Handle metadata for clonotype consolidation
+#'
+#' For duplicated clonotypes with different metadata values,
+#' concatenates unique values with semicolons.
+#'
+#' @param meta Metadata data frame
+#' @param meta.carry Variables to process
+#'
+#' @return Processed metadata data frame
+#' @keywords internal
 #' @importFrom stringr str_sort
 meta.handler <- function(meta, meta.carry) {
-  unique.clones <- unique(meta[,"CTaa"])
-  duplicated.clones <- na.omit(unique(meta[,"CTaa"][which(duplicated(meta[,"CTaa"]))]))
-  new.meta <- NULL
-  for (i in seq_along(duplicated.clones)) {
-    meta.tmp <- meta[meta[,"CTaa"] == duplicated.clones[i], ]
-    concat.strings <- lapply(meta.carry, function(x) {
-            paste0(str_sort(unique(na.omit(meta.tmp[,x]))), collapse = ";")
-    })
-    new.meta <- rbind(new.meta, unlist(concat.strings))
-  }
-  old.meta <- meta[meta[,"CTaa"] %!in% duplicated.clones, meta.carry]
-  if (length(new.meta) != 0) {
-    colnames(new.meta) <- meta.carry
-    total.meta <- rbind(old.meta, new.meta)
-  } else {
-    total.meta <- old.meta
-  }
-  return(total.meta)
+    unique.clones <- unique(meta[, "CTaa"])
+    duplicated.clones <- na.omit(unique(
+        meta[, "CTaa"][which(duplicated(meta[, "CTaa"]))]
+    ))
+    new.meta <- NULL
+
+    for (i in seq_along(duplicated.clones)) {
+        meta.tmp <- meta[meta[, "CTaa"] == duplicated.clones[i], ]
+        concat.strings <- lapply(meta.carry, function(x) {
+            paste0(str_sort(unique(na.omit(meta.tmp[, x]))), collapse = ";")
+        })
+        new.meta <- rbind(new.meta, unlist(concat.strings))
+    }
+
+    old.meta <- meta[meta[, "CTaa"] %!in% duplicated.clones, meta.carry]
+    if (length(new.meta) != 0) {
+        colnames(new.meta) <- meta.carry
+        total.meta <- rbind(old.meta, new.meta)
+    } else {
+        total.meta <- old.meta
+    }
+    return(total.meta)
 }
